2b2cb4c5af704ac043943f267632236903ceabd3
CARLOS EDUARDO SANTOS ALMEIDA <cadu.almeida.cc@gmail.com>
2021-02-18 16:23:11 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Carlos Eduardo',
+                partido: 'CED',
+                fotos: [
+                    {url:'avatar.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Debora Larissa',
+                partido: '1DF',
+                fotos: [
+                    {url:'avatar.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '07',
+                nome: 'Carlos Eduardo',
+                partido: 'CED',
+                vice: 'Mulher-Maravilha',
+                fotos: [
+                    {url:'avatar.png', legenda: 'Prefeito'},
+                    {url:'avatar.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Debora Larissa',
+                partido: '1DF',
+                vice: 'Harry Styles',
+                fotos: [
+                    {url:'avatar.png', legenda: 'Prefeito'},
+                    {url:'avatar.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Stefhanie OLiveira',
+                partido: 'S2S2',
+                vice: 'Taylor Swift',
+                fotos: [
+                    {url:'avatar.png', legenda: 'Prefeito'},
+                    {url:'avatar.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
